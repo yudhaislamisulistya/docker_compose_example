13aa0fbb4b0279581e75d197ce83e0e614d5f997
Yudha Islami Sulistya <yudhamaster77@gmail.com>
2025-04-13 21:22:51 +0700

    fix: update database connection string to include password

--- backend/db.go
+++ backend/db.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"log"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -11,13 +12,27 @@ var db *sql.DB
 
 func InitDB() {
 	var err error
-	dsn := "root:@tcp(localhost:3306)/academic"
+
+	// Membaca variabel lingkungan
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+
+	// Membuat Data Source Name (DSN) menggunakan variabel lingkungan
+	dsn := dbUser + ":" + dbPassword + "@tcp(" + dbHost + ":" + dbPort + ")/" + dbName
+
+	// Membuka koneksi ke database
 	db, err = sql.Open("mysql", dsn)
 	if err != nil {
 		log.Fatal("Gagal koneksi ke database:", err)
 	}
 
+	// Memeriksa koneksi
 	if err = db.Ping(); err != nil {
 		log.Fatal("Tidak bisa terhubung ke database:", err)
 	}
+
+	log.Println("Berhasil terhubung ke database!")
 }
